d25dc19c372329aadf813b7a5508ad02d34c0175
ksfhjlas <science_yaba32204@softbank.ne.jp>
2017-10-14 01:34:36 +0900

    add_req
    add requests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pya3rt
+import random
 
 def main(argv):
   apikey = "t3yTIONK5tfoySZIqukmblGZ7BXZgYO3"
   client = pya3rt.TextSuggestClient(apikey)
+
+  a="我"
+  b=[]
+  while True:
+    c=client.text_suggest(a)
+    for i in c["suggestion"]:
+      print i
+      b.append(i)
+    a=random.shuffle(b)
+    b=[]
+
 
 
   for i, v in enumerate(argv):
